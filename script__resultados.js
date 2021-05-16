b711d9163185484ede6441558a25b3da327f15dc
Llunabp <83987819+Llunabp@users.noreply.github.com>
2021-05-16 13:50:16 +0200

    Add files via upload

---
+++ script__resultados.js
@@ -0,0 +1,9 @@
+function mostrar1(){
+    document.getElementById("titulo").innerHTML = "Staphylococus aureus:"
+    document.getElementById("texto").innerHTML = "Podemos cultivar una colonia aislada en el agar sal manitol para comprobar si es este el coco que observamos."
+}
+
+function mostrar2(){
+    document.getElementById("titulo").innerHTML = "Neissenia gonorrhoeae:"
+    document.getElementById("texto").innerHTML = "Podemos cultivar una colonia aislada en el medio Thayer Martin modificado para comprobar si es este el coco que observamos."
+}
